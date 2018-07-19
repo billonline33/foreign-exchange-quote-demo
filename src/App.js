9a3fbed31824ecb76ab953608037faa2cea4cc8c
Bill Huang <billonline33@hotmail.com>
2018-07-19 15:02:01 +1000

    feat(UI): convert html page into react components
    add style.css
    add bootstrap.min.css
    format input.js and quote.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Input from "./components/Layout/InputContainer/Input";
 import Quote from "./components/Layout/QuoteContainer/Quote";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="capture container row">
         <Input />
         <Quote />
       </div>
